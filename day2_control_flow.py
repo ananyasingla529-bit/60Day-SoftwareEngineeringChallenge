666c882b9f6c0d4c6716d3886e310337379b6086
ananyasingla529-bit <ananyasingla529@gmail.com>
2026-09-01 23:43:13 +0530

    Refactor marks input with error handling and validation

--- day2_control_flow.py
+++ day2_control_flow.py
@@ -1,9 +1,14 @@
-marks = int(input("Enter your marks: "))
-if(marks>90):
-  print("A")
-elif(marks>75):
-  print("B")
-elif(marks>50):
-  print("C")
-else:
-  print("Fail")
+try:
+  marks = float(input("Enter your marks: "))
+  if(marks<0 or marks>100):
+    print("Invalid marks! Please enter marks between 0 and 100.")
+  elif(marks>90):
+    print("A \nPassed")
+  elif(marks>75):
+    print("B \nPassed")
+  elif(marks>50):
+    print("C\nPassed")
+  else:
+    print("Fail\nFailed")
+except ValueError:
+  print("Invalid input! Please enter a number.")
